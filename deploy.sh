704ec910feb2aaf0d6f61014bb52e266d921458d
clairv123 <105845191+clairv123@users.noreply.github.com>
2022-05-19 12:37:35 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ gcloud functions deploy probability_calculator \
 --entry-point=main \
 --memory=2048MB \
 --runtime=python38 \
---service-account=sandbox-clairv@appspot.gserviceaccount.com \
+--service-account=
 --trigger-http \
 --timeout=180
 
@@ -25,24 +25,6 @@ gcloud functions deploy probability_calculator_fs_trigger \
 --memory=2048MB \
 --timeout=180 \
 --trigger-event=providers/cloud.firestore/eventTypes/document.write \
---trigger-resource="projects/sandbox-clairv/databases/(default)/documents/webevent/{sessionid}"
+--trigger-resource="
 
 
-# SANDBOX CLAIR
-gcloud builds submit --tag eu.gcr.io/sandbox-clairv/probability-calculator
-
-
-gcloud run deploy  probability-calculator  --image eu.gcr.io/sandbox-clairv/probability-calculator  --platform=managed\
-    --service-account=510185108287-compute@developer.gserviceaccount.com\
-    --update-labels=creator=davy_van_der_horst,application=probability-calculator\
-    --memory=8Gi --timeout=3600 --region=europe-west1 --cpu=4
-
-
-# JODP STREAM
-gcloud builds submit --tag eu.gcr.io/jodp-stream/probability-calculator
-
-
-gcloud run deploy  probability-calculator  --image eu.gcr.io/jodp-stream/probability-calculator  --platform=managed\
-    --service-account=jodp-stream@appspot.gserviceaccount.com\
-    --update-labels=creator=davy_van_der_horst,application=probability-calculator\
-    --memory=8Gi --timeout=3600 --region=europe-west1 --cpu=4
